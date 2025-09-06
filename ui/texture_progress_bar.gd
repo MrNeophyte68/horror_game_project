d4a54a673fae3199a18ca8dc075bb8867e5f56d6
MrNeophyte68 <akhan.mehmet@sozen.ca>
2025-09-05 22:08:13 -0400

    first floor change

--- ui/texture_progress_bar.gd
+++ ui/texture_progress_bar.gd
@@ -3,4 +3,10 @@ extends TextureProgressBar
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	modulate.a = 0.35
+	modulate.a = 0.0
+
+	#await get_tree().create_timer(11.0, false).timeout
+
+	# Create a tween
+	var tween = create_tween()
+	tween.tween_property(self, "modulate:a", 0.35, 2.0) # fade over 2 seconds
